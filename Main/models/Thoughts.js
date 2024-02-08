7bf473a6706e61ba9b1ec09774fe0da9be1e5580
Toan <nguyentoan0275@gmail.com>
2024-02-08 14:19:36 -0500

    created the schemas/models

---
+++ Main/models/Thoughts.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtsSchema = new Schema({
+  thoughtText: {
+    // String
+    // Required
+    // Must be between 1 and 280 characters
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    // String
+    // Required
+    type: String,
+    required: true,
+  },
+  //   Array of nested documents created with the reactionSchema
+  reactions: [reactions],
+  createdAt: {
+    // Date
+    // Set default value to the current timestamp
+    // Use a getter method to format the timestamp on query
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Thoughts = model("thoughts", thoughtsSchema);
+
+module.exports = Thoughts;
--- Main/models/User.js
+++ Main/models/User.js
@@ -1,1 +1,43 @@
-const { Schema, model} = require('mongoose')
+const { Schema, model } = require("mongoose");
+
+var validateEmail = function (email) {
+  // using regex to validate email inputs
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailRegex.test(email);
+};
+
+// schmea to create the User model
+const userSchema = new Schema({
+  // username
+  // String
+  // Unique
+  // Required
+  // Trimmed
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  // email
+  // String
+  // Required
+  // Unique
+  // Must match a valid email address (look into Mongoose's matching validation
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, "Please enter a valid email address"],
+  },
+  // thoughts
+  // Array of _id values referencing the Thought model
+  thoughts: [thoughtsSchema],
+  // friends
+  // Array of _id values referencing the User model (self-reference)
+  friends: [friends],
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
